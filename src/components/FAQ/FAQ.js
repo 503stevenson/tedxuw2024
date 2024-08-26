e9cadf85884b1f9a650a52b02ba065ec477b191d
Iris <irismo1009@gmail.com>
2024-08-26 03:31:56 -0400

    updated faq page without image

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -9,19 +9,21 @@ const FAQ = () => {
             FAQS
         </p>
         <div className='questions'>
-            <Question question="Why you?" answer="idk"/>
-            <Question question="When will you get this finished? longgggggggggggggggg ggggggggggggg ggggggggggggggggg quesitonnnnnnnnnnnnn" 
-            answer="idk but this is also a reallyyyyyy long answer with lots of words idk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of words idk but this is also a reallyyyyyy long answer with lots of words idk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words
-            idk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of wordsidk but this is also a reallyyyyyy long answer with lots of words"/>
-            <Question question="Why you?" answer="idk"/>
-            <Question question="Why you?" answer="idk"/>
-            <Question question="Why you?" answer="idk"/>
+            <Question question="Where and when is TEDxUW 2023 happening?" 
+            answer="The TEDxUW 2024 conference will be happening on October 6, 2024 from 9AM - 5PM EST! The conference will be taking place at the University of Waterloo Theatre of the Arts.  Register to secure your spot today!"/>
+            <Question question="Will this year’s conference be in person or virtual?" 
+            answer="This year’s conference will be in-person! The TEDxUW 2023 conference will be taking place at the University of Waterloo Theatre of the Arts. This is located at the University of Waterloo, Ring Rd, Waterloo, ON N2L 3G1."/>
+            <Question question="What’s the event breakdown of TEDxUW?" 
+            answer="A TEDxUW conference is a local gathering where live TED-like presentations are shared with a community. These conferences consist of talks from outstanding speakers and performers, and also include fun activities the audience can engage in. The theme for TEDxUW 2024 is: In The Odd Chance. Through this year’s theme, we celebrate the courage of embracing what is uncomfortable strategically to meet challenges head-on and unlock our full potential."/>
+            <Question question="How can I get more involved with TEDxUW" 
+            answer="We love having new people join our TEDxUW family of change-makers and we have plenty of opportunities for you to get involved. You can be a part of the movement in many ways, such as:
+Apply to be a speaker or nominate a potential speaker
+Applying to join our core team
+Being one of our powerhouse volunteers (our greatest pride!)
+Be our lens to the world and cover our event as a media partner
+Partner with us as an organization, club, or individual
+Become a sponsor
+If you want to get involved, email us at outreach@tedxuw.com and we can help you out!"/>
         </div>
     </div>
   )
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,6 +4,7 @@ import './Question.css';
 
 const Question = ({question, answer}) => {
     const [isOpen, setIsOpen] = useState(false)
+    const answerLines = answer.split('\n');
 
   return (
     <div className={`qanda ${isOpen ? 'open' : ''}`}>
@@ -11,7 +12,22 @@ const Question = ({question, answer}) => {
             <div className='question'>{question}</div>
             <img src={faq_star} alt="Toggle Answer" onClick={() => setIsOpen(!isOpen)} className="star"/>
         </div>
-        {isOpen && <p className='answer'>{answer}</p>}
+        {isOpen && answerLines.length > 1 && (
+          <div className='answer'>
+            <p>{answerLines[0]}</p>
+            <ul>
+            {answerLines.slice(1, -1).map((line, index) => ( 
+              <li key={index}>{line}</li>
+            ))}
+            </ul>
+            <p>{answerLines[answerLines.length - 1]}</p>
+          </div>
+        )}
+        {isOpen && answerLines.length == 1 && (
+          <div className='answer'>
+            <p>{answerLines[0]}</p> 
+          </div>
+        )}
     </div>
   )
 }
