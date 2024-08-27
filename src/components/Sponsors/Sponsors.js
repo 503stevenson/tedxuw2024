f1c9d76f6d62b7dae413b560be0eb7c88880c012
Iris <irismo1009@gmail.com>
2024-08-27 16:52:51 -0400

    removed sponsor page from navbar and edit website title and favico

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import './Sponsors.css'
 import {
-    AEF,
-    bullfrogpowered,
-    CIGI,
-    Extempra,
-    WUSA,
-    plat_star,
-    gold_stars,
-    silver_stars
+  AEF,
+  bullfrogpowered,
+  CIGI,
+  Extempra,
+  WUSA,
+  plat_star,
+  gold_stars,
+  silver_stars
 } from '../../assets/index'
 
 const Sponsors = () => {
   return (
     <div className="sponsors-content">
-        <p className="title">
-            SPONSORS
-        </p>
-        <div className='sponsors'>
-            <img src={Extempra} alt="Extempra logo"/>
-        </div>
+      <p className="title">
+        SPONSORS
+      </p>
+      <div className='plat-sponsors'>
+        <img src={Extempra} alt="Extempra logo" />
+      </div>
+      <div className='gold-sponsors'>
+        <img src={CIGI} alt="Centre for International Governance Innovation logo" />
+      </div>
+      <div className='silver-sponsors'>
+        <img src={AEF} alt="AEF logo" />
+        <img src={WUSA} alt="WUSA logo" />
+      </div>
+      <div className='bronze-sponsors'>
+        <img src={bullfrogpowered} alt="bullfrogpowered logo" />
+      </div>
     </div>
   )
 }
