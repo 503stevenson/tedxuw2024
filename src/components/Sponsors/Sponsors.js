878ea3b105292f305bba6fcd83d2fc3f45892ac1
Iris <irismo1009@gmail.com>
2024-08-27 18:27:23 -0400

    finished sponsor page and mobile faq page

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -18,16 +18,22 @@ const Sponsors = () => {
         SPONSORS
       </p>
       <div className='plat-sponsors'>
+        <p className='sponsor-tier'>Platinum Sponsors</p>
         <img src={Extempra} alt="Extempra logo" />
       </div>
       <div className='gold-sponsors'>
+        <p className='sponsor-tier'>Gold Sponsors</p>
         <img src={CIGI} alt="Centre for International Governance Innovation logo" />
       </div>
       <div className='silver-sponsors'>
-        <img src={AEF} alt="AEF logo" />
-        <img src={WUSA} alt="WUSA logo" />
+        <p className='sponsor-tier'>Silver Sponsors</p>
+        <div className='silver-images'>
+          <img src={AEF} alt="AEF logo" />
+          <img src={WUSA} alt="WUSA logo" />
+        </div>
       </div>
       <div className='bronze-sponsors'>
+        <p className='sponsor-tier'>Bronze Sponsors</p>
         <img src={bullfrogpowered} alt="bullfrogpowered logo" />
       </div>
     </div>
