f7a08c4f8bf485779c8cdc5386e00148dca68091
Michael Stevenson <503stevenson@gmail.com>
2024-09-10 22:01:02 -0400

    fixed mistake with 2 classes having same name.

--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -6,8 +6,8 @@ const Speaker = ({ name, title, about, alignLeft, image }) => {
         <>
             <div className="speakerbig">
                 {alignLeft &&
-                    <div className="image-container">
-                        <img className="profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
+                    <div className="speaker-image-container">
+                        <img className="speaker-profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
                     </div>
                 }
                 <div className="details">
@@ -18,14 +18,14 @@ const Speaker = ({ name, title, about, alignLeft, image }) => {
                     <p className='speakerabout'>{about}</p>
                 </div>
                 {!alignLeft &&
-                    <div className="image-container">
-                        <img className="profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
+                    <div className="speaker-image-container">
+                        <img className="speaker-profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
                     </div>
                 }
             </div>
             <div className="speakersmall">
-                <div className="image-container">
-                    <img className="profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
+                <div className="speaker-image-container">
+                    <img className="speaker-profile-pic" src={require(`../Speakers/photos/${image}`)} alt={name} />
                 </div>
                 <h3 className="name">
                     {name} 
