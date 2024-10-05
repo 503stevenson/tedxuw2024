80fd66fe9f9afadf1fbe745ae4a69bc9fd0576fd
Gavneet <gavneet.bhandal@gmail.com>
2024-10-05 11:05:04 -0400

    added uw logo

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,5 +1,5 @@
-import React from 'react'; 
-import './Sponsors.css';
+import React from "react";
+import "./Sponsors.css";
 import {
   AEF,
   bullfrogpowered,
@@ -13,8 +13,9 @@ import {
   Riptide,
   MEF,
   Fantuan,
-} from '../../assets/index';
-import ACCOMMOD8U_Logo from '../../assets/ACCOMMOD8U_Logo.png';
+} from "../../assets/index";
+import ACCOMMOD8U_Logo from "../../assets/ACCOMMOD8U_Logo.png";
+import UW from "../../assets/UniversityOfWaterloo_logo_horiz_rgb.jpg";
 
 const Sponsors = () => {
   return (
@@ -23,10 +24,18 @@ const Sponsors = () => {
       <div className="diamond-sponsors">
         <p className="sponsor-tier">Diamond Sponsors</p>
         <div className="silver-images">
-          <a href="https://uwaterloo.ca/math-endowment-fund/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://uwaterloo.ca/math-endowment-fund/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={MEF} alt="MEF logo" />
           </a>
-          <a href="https://www.extempra.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.extempra.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={Extempra} alt="Extempra logo" />
           </a>
         </div>
@@ -35,8 +44,22 @@ const Sponsors = () => {
       <div className="plat-sponsors">
         <p className="sponsor-tier">Platinum Sponsors</p>
         <div className="silver-images">
-          <a href="http://riptidecreative.ca/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="http://riptidecreative.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={Riptide} alt="Riptide logo" />
+          </a>
+        </div>
+
+        <div className="silver-images">
+          <a
+            href="https://uwaterloo.ca/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img style={{ borderRadius: "30px" }} src={UW} alt="UW logo" />
           </a>
         </div>
       </div>
@@ -45,16 +68,35 @@ const Sponsors = () => {
         <p className="sponsor-tier">Gold Sponsors</p>
         <div className="gold-logos">
           <div className="top-logos">
-            <a href="https://www.cigionline.org/" target="_blank" rel="noopener noreferrer">
-              <img src={CIGI} alt="Centre for International Governance Innovation logo" />
+            <a
+              href="https://www.cigionline.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={CIGI}
+                alt="Centre for International Governance Innovation logo"
+              />
             </a>
-            <a href="https://fantuan.ca/delivery/en/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://fantuan.ca/delivery/en/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={Fantuan} alt="Fantuan logo" />
             </a>
           </div>
           <div className="bottom-logo">
-            <a href="https://www.accommod8u.com/" target="_blank" rel="noopener noreferrer">
-              <img className="accommod8u-logo" src={ACCOMMOD8U_Logo} alt="Accommod8u logo" />
+            <a
+              href="https://www.accommod8u.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="accommod8u-logo"
+                src={ACCOMMOD8U_Logo}
+                alt="Accommod8u logo"
+              />
             </a>
           </div>
         </div>
@@ -63,13 +105,25 @@ const Sponsors = () => {
       <div className="silver-sponsors">
         <p className="sponsor-tier">Silver Sponsors</p>
         <div className="silver-images">
-          <a href="https://uwaterloo.ca/arts-endowment-fund/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://uwaterloo.ca/arts-endowment-fund/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={AEF} alt="AEF logo" />
           </a>
-          <a href="https://wusa.ca/about/your-money/funding/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://wusa.ca/about/your-money/funding/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={WUSA} alt="WUSA logo" />
           </a>
-          <a href="https://uwaterloo.ca/school-of-accounting-and-finance/accounting-and-finance-endowment-fund-afef" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://uwaterloo.ca/school-of-accounting-and-finance/accounting-and-finance-endowment-fund-afef"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={AFEF} alt="AFEF logo" />
           </a>
         </div>
@@ -78,7 +132,11 @@ const Sponsors = () => {
       <div className="bronze-sponsors">
         <p className="sponsor-tier">Bronze Sponsors</p>
         <div className="silver-images">
-          <a href="https://bullfrogpower.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://bullfrogpower.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={bullfrogpowered} alt="bullfrogpowered logo" />
           </a>
         </div>
